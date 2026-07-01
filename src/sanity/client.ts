cacf8ff33aa7569a445cdc400decd5b11f3ffb82
jwabo <joelwabo@yahoo.fr>
2026-06-30 21:59:04 -0600

    Update client.ts

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "rp9nlgk5",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2026-06-10",
   useCdn: true, // true for fast CDN reads, defineLive overrides this for drafts/editing
 });
